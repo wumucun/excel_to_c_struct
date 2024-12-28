bea9890378f4c7e2cfede4ba6e6341a0d0ba9dd1
wumucun <yixia1999@gmail.com>
2024-12-28 18:30:32 +0800

    添加Python工程

---
+++ main.py
@@ -0,0 +1,52 @@
+import pandas as pd
+
+def get_column_index(df, column_name):
+    return df.columns.get_loc(column_name)
+
+def process_df(df, start_row, end_row, start_col, end_col, layer):
+    space = "    " * layer
+    if layer > 10:
+        print("递归太深.")
+        return
+    while start_row < end_row:
+        if start_col < end_col:
+            name = df.iloc[start_row, start_col]
+            type = df.iloc[start_row, start_col + 1]
+            length = df.iloc[start_row, start_col + 2]
+            # int 类型
+            if type != 'struct' and length == 1:
+                print(f"{space}.{name} = {df.iloc[start_row]['value']}")
+                start_row += 1
+                if start_row > end_row:
+                    return
+            # 数组类型
+            elif type != 'struct' and length > 1:
+                print(f"{space}.{name} =")
+                print(f"{space}{{")
+                while length > 0:
+                    print(f"{space}{df.iloc[start_row]['value']}")
+                    length -= 1
+                    start_row += 1
+                print(f"{space}}}, ")
+                if start_row > end_row:
+                    return
+            # 结构体类型
+            elif type == 'struct':
+                print(f"{space}.{name} =")
+                print(f"{space}{{")
+                layer += 1
+                process_df(df, start_row, start_row + int(length), start_col + 3, end_col, layer)
+                layer -= 1
+                start_row += int(length)
+                print(space + '}')
+        else:
+            return
+
+
+
+if __name__ == '__main__':
+    df = pd.read_excel('demo.xlsx', sheet_name='Sheet1')
+    end_col = df.columns.get_loc('mean')
+    layer = 0
+    process_df(df, 0, len(df), 0, end_col, layer)
+    
