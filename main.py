dea09d1b19199ed32a31df8979de2e0b182b097d
wumucun <yixia1999@gmail.com>
2024-12-29 12:24:52 +0800

    不需要关注struct的长度，通过遍历查询

--- main.py
+++ main.py
@@ -35,7 +35,14 @@ def process_df(df, start_row, end_row, start_col, end_col, layer):
                 print(f"{space}.{name} =")
                 print(f"{space}{{")
                 layer += 1
-                process_df(df, start_row, start_row + int(length), start_col + 3, end_col, layer)
+                # 查询结构体结束行的index
+                i = start_row + 1
+                while i < end_row:
+                    if isinstance(df.iloc[i, start_col], str):
+                        break
+                    else:
+                        i += 1
+                process_df(df, start_row, i, start_col + 3, end_col, layer)
                 layer -= 1
                 start_row += int(length)
                 print(space + '}')
